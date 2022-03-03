2a7d05ea516a25ee98023c75d6c70550125eb6fc
Alexander Marquez <alex18escorpio@gmail.com>
2022-03-03 18:39:53 -0500

    form products

--- src/App.js
+++ src/App.js
@@ -1,24 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import { productData } from "./data";
 
 function App() {
+  const [showCard, setCard] = useState("none");
+  function ShowCards() {
+    setCard(showCard === "none" ? "grid" : "none");
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1>Precios</h1>
+      <button className="btn" onClick={ShowCards}>
+        Ver Productos
+      </button>
+      <section className="cards" style={{ display: showCard }}>
+        {productData.map((product, index) => {
+          return (
+            <CardProducto
+              key={index}
+              img={product.img}
+              title={product.title}
+              price={product.price}
+              color={
+                typeof product.color === "undefined" ? "#fbedf9" : product.color
+              }
+            />
+          );
+        })}
+      </section>
     </div>
+  );
+}
+
+function CardProducto(props) {
+  return (
+    <section className="card" style={{ background: props.color }}>
+      <img className="card__img" src={props.img} alt="" />
+      <div className="information">
+        <h3 className="card__title">{props.title}</h3>
+        <h4>{`$ ${props.price}`}</h4>
+      </div>
+    </section>
   );
 }
 
